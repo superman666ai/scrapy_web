7f4135bc06f22c59e339b238c32565289a70aa24
keepingoner <keepingoner@163.com>
2019-03-11 21:46:56 +0800

    data

--- scrapyData/getData/getData/spiders/test.py
+++ scrapyData/getData/getData/spiders/test.py
@@ -1,17 +1,16 @@
 import scrapy
-
-
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
         # "https://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
         # "https://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
-        "https://www.baidu.com/"
+        # "https://www.baidu.com/"
+        "http://caipiao.163.com/"
     ]
 
     def parse(self, response):
-        print(response.body,"+++++")
+        # print(response.body, "+++++")
         filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
+        with open("../getData/data/" + filename + ".txt", 'wb') as f:
             f.write(response.body)
